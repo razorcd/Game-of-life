a6d31a6d93a45ac2ec6197cf281e3ec807f3c1ed
Cristian <cristian.dugacicu@ubitricity.com>
2017-01-07 23:38:44 +0100

    adds 1st rule

--- ruby/app/game_of_life.rb
+++ ruby/app/game_of_life.rb
@@ -1,10 +1,45 @@
+require "pry"
+
 class GameOfLife
 
-    def initialize x, y
-        @board = [[false]*x]*y
+    def initialize board
+        @board = board
+        @length = board.first.size
+        @width = board.size
     end
 
     def board
         @board
+    end
+
+    def tick
+        @length.times do |i|
+            @width.times do |j|
+                puts "#{i}, #{j}"
+                calculate!(i,j)
+            end            
+        end
+    end
+
+    private
+
+    def calculate!(x,y)
+        alive_neighbours_count = alive_neighbours_count(x,y)
+        if (alive_neighbours_count < 2) then set(x,y,false) end
+    end
+
+    def alive_neighbours_count(x,y)
+        [get(x-1,y-1), get(x,y-1), get(x+1,y-1),
+         get(x-1,y),               get(x+1,y),
+         get(x-1,y+1), get(x,y+1), get(x+1,y+1)
+    ].select{|v| v==true}.count
+    end
+
+    def get(x,y)
+        @board&.[](x)&.[](y)
+    end
+
+    def set(x,y,val)
+        @board[x][y] = val
     end
 end
--- ruby/spec/game_of_life_spec.rb
+++ ruby/spec/game_of_life_spec.rb
@@ -1,11 +1,26 @@
 require_relative "../app/game_of_life.rb"
 
 RSpec.describe GameOfLife do
-    it "should initialize with a board of size x,y" do
-        expect(GameOfLife.new(5,4)).not_to eq(nil)
+    it "should initialize with a board" do
+        expect(GameOfLife.new([[false, true], [true, false]])).not_to eq(nil)
     end
 
     it "should return the current state of the board" do
-        expect(GameOfLife.new(2,3).board).to eq([[false, false], [false, false], [false, false]])
+        game = GameOfLife.new([[false, false], [false, false], [false, false]])
+        expect(game.board).to eq([[false, false], [false, false], [false, false]])
     end
+
+    it "should generate: Any live cell with fewer than two live neighbours dies" do
+        game = GameOfLife.new([
+            [false, false, false], 
+            [false, true, false], 
+            [false, false, false]
+        ])
+        game.tick
+        expect(game.board).to eq([
+            [false, false, false], 
+            [false, false, false], 
+            [false, false, false]
+        ])
+    end 
 end
