94b98fa5125b43e7152e74447f47657682ef7bd3
Cristian <cristian.dugacicu@ubitricity.com>
2017-01-08 18:16:53 +0100

    adds automatic test cases

--- ruby/spec/game_of_life_spec.rb
+++ ruby/spec/game_of_life_spec.rb
@@ -10,17 +10,48 @@ RSpec.describe GameOfLife do
         expect(game.board).to eq([[false, false], [false, false], [false, false]])
     end
 
-    it "should generate: Any live cell with fewer than two live neighbours dies" do
-        game = GameOfLife.new([
-            [false, false, false], 
-            [false, true, false], 
-            [false, false, false]
-        ])
-        game.tick
-        expect(game.board).to eq([
-            [false, false, false], 
-            [false, false, false], 
-            [false, false, false]
-        ])
-    end 
+    describe "rules" do
+        test_cases = {
+            all_false: {
+                title: "All false.",
+                input: 
+                    [
+                        [false, false, false], 
+                        [false, false, false], 
+                        [false, false, false]
+                    ],
+                output:    
+                    [
+                        [false, false, false], 
+                        [false, false, false], 
+                        [false, false, false]
+                    ]
+            },
+            middle_true: {
+                title: "Middle true",
+                input: 
+                    [
+                        [false, false, false], 
+                        [false, true,  false], 
+                        [false, false, false]
+                    ],
+                output:    
+                    [
+                        [false, false, false], 
+                        [false, false, false], 
+                        [false, false, false]
+                    ]
+            },
+        }
+
+        test_cases.values.each do |test_case|
+            it "should run test case: #{test_case[:title]}" do
+                game = GameOfLife.new(test_case[:input])
+                game.tick
+                expect(game.board).to eq(test_case[:output])
+            end
+        end
+
+    end
+
 end
