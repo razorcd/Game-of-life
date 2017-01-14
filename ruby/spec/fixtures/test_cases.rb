89ebb51e8c9b1aef09380ac3d433d611b7bee23c
Cristian <cristian.dugacicu@ubitricity.com>
2017-01-14 17:39:30 +0100

    adds complete test cases

--- ruby/spec/fixtures/test_cases.rb
+++ ruby/spec/fixtures/test_cases.rb
@@ -126,5 +126,67 @@ module TestCases
                         [false, false, false]
                     ]
             },
+
+            five_neighbours_false: {
+                title: "5 neighbours_false",
+                input: 
+                    [
+                        [true,  true, true], 
+                        [false, false, false], 
+                        [true,  false, true]
+                    ],
+                output:    
+                    [
+                        [false, true, false], 
+                        [false, false, false], 
+                        [false, false, false]
+                    ]
+            },
+            five_neighbours_true: {
+                title: "5 neighbours_true",
+                input: 
+                    [
+                        [true,  true,  true], 
+                        [false, true,  false], 
+                        [true,  false, true]
+                    ],
+                output:    
+                    [
+                        [true,  true,  true], 
+                        [false, false, false], 
+                        [false, false, false]
+                    ]
+            },
+
+            all_neighbours_false: {
+                title: "all neighbours_false",
+                input: 
+                    [
+                        [true, true,  true], 
+                        [true, false, true], 
+                        [true, true,  true]
+                    ],
+                output:    
+                    [
+                        [true,  false, true], 
+                        [false, false, false], 
+                        [true,  false, true]
+                    ]
+            },
+            all_neighbours_true: {
+                title: "all neighbours_true",
+                input: 
+                    [
+                        [true, true, true], 
+                        [true, true, true], 
+                        [true, true, true]
+                    ],
+                output:    
+                    [
+                        [true,  false, true], 
+                        [false, false, false], 
+                        [true,  false, true]
+                    ]
+            },
         }
 end
