475e80397d2a8147550003e8aba20bc2f6743ca9
Cristian <cristian.dugacicu@ubitricity.com>
2017-01-23 23:10:25 +0100

    adds documentation

--- ruby/app/game_of_life.rb
+++ ruby/app/game_of_life.rb
@@ -3,22 +3,34 @@ require "json"
 
 class GameOfLife
 
+    # Initialize game with a predefined board state.
+    # board [Array<Array<Boolean>>] The board defined by an Array of Arrays. 
+    #                               Cell values are booleans. allive = true, dead =false.
+    #        E.g: [
+    #                [true,  true,  true,  false], 
+    #                [false, false, false, true], 
+    #                [true,  false, false, true],
+    #                [true,  true,  true,  false],
+    #             ]
     def initialize board
         @board = board
         @length = board.first.size
         @width = board.size
     end
 
+    # Returns the board game in currate state
     def board
         @board
     end
 
+    # Generates a new itteration of the game
     def tick
         @board = generate_next_board
     end
 
     private
 
+    # Returns the value of the next itteration for the entire board.
     def generate_next_board
         temp_board = JSON.parse(([[nil]*@length]*@width).to_json)
         @length.times do |i|
@@ -29,6 +41,7 @@ class GameOfLife
         temp_board
     end
 
+    # Returns the value of the next itteration for once cell defined by x,y coordinates.
     def generate_next_cell(x,y)
         alive_neighbours_count = alive_neighbours_count(x,y)
         if (alive_neighbours_count < 2) then return false end         #rule 1
@@ -38,13 +51,15 @@ class GameOfLife
         return @board[x][y]
     end
 
+    # Counts the allive neighbours that surround the cell defined by the x,y coordinates.
     def alive_neighbours_count(x,y)
         [get(x-1,y-1), get(x,y-1), get(x+1,y-1),
          get(x-1,y),               get(x+1,y),
          get(x-1,y+1), get(x,y+1), get(x+1,y+1)
-    ].select{|v| v==true}.count
+        ].select{|v| v==true}.count
     end
 
+    # Gets the value of a cell by it's x,y coordinates. Returns nil if coordianates are off.
     def get(x,y)
         return nil if (x<0 || y<0 || x>@length-1 || y>@width-1)
         @board&.[](x)&.[](y)
