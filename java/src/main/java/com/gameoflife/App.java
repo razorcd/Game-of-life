2e08f842339154cd4a44363a55c8f38fe0fe872e
Razor <razorcd@yahoo.com>
2017-01-29 20:31:55 +0100

    java init

---
+++ java/src/main/java/com/gameoflife/App.java
@@ -0,0 +1,11 @@
+package com.gameoflife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
