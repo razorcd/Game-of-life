ed58495eb5267b65236a5d349ba43ab5edd55757
Razor <razorcd@yahoo.com>
2017-04-01 12:36:29 +0200

    adds JS: cell with no neighbours

--- javascript/lib/GameOfLife.js
+++ javascript/lib/GameOfLife.js
@@ -4,11 +4,22 @@ module.exports = (function() {
 
   /**
    * Initializes the game.
-   * @param {boolean[][]} matrix 
+   * @param {boolean[][]} matrix
    */
   var GameOfLife = function(matrix) {
     this._matrix = matrix;
   };
+
+  /**
+   * Generates a new game iteration.
+   */
+  GameOfLife.prototype.tick = function() {
+    for (var i = 0; i < this._matrix.length; i++) {
+      for (var j = 0; j < this._matrix[0].length; j++) {
+        this._matrix[i][j] = false;
+      }
+    }
+  }
 
   /**
    * Returns the current matrix.
--- javascript/spec/GameOfLife.spec.js
+++ javascript/spec/GameOfLife.spec.js
@@ -8,12 +8,40 @@ describe('GameOfLife', function() {
   });
 
   describe('initialization', function() {
-
     it('should set the initial matrix', function() {
       var game = new GameOfLife([[true], [false]]);
       expect(game.getMatrix()).toEqual([[true], [false]]);
     });
+  });
 
+  describe('rules', function() {
+    it('dead cell with no neighbours', function() {
+      var game = new GameOfLife([
+        [false, false, false],
+        [false, false, false],
+        [false, false, false]
+      ]);
+      game.tick();
+      expect(game.getMatrix()).toEqual([
+          [false, false, false],
+          [false, false, false],
+          [false, false, false]
+        ]);
+    });
+
+    it('alive cell with no neighbours', function() {
+      var game = new GameOfLife([
+        [false, false, false],
+        [false, true, false],
+        [false, false, false]
+      ]);
+      game.tick();
+      expect(game.getMatrix()).toEqual([
+          [false, false, false],
+          [false, false, false],
+          [false, false, false]
+        ]);
+    });
   });
 
 });
